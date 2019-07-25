ff0961154b98fd263545b52fbdf41d98e4425efc
allef-douglas <allef.douglas@aftisolucoes.com.br>
2019-07-25 16:59:50 -0300

    MacOS support

--- src/utils/Terminal.java
+++ src/utils/Terminal.java
@@ -1,18 +1,38 @@
 package utils;
 
 public class Terminal {
-    public static void exe(String path) throws Exception {
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "code " + path);
+    public static void executeWindows(String command) throws Exception {
+        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", command);
+        pb.start();
+    }
+
+    public static void executeMac(String command) throws Exception {
+        ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
         pb.start();
     }
     
     public static void openVSCode(String path)  throws Exception {
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "code " + path);
-        pb.start();
+        if (OSValidator.isWindows()) {
+            executeWindows("code " + path);
+            return;
+        }
+
+        if (OSValidator.isMac()) {
+            executeMac("code " + path);
+            return;
+        }
+
     }
     
     public static void openFolder(String path)  throws Exception {
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start " + path);
-        pb.start();
+        if (OSValidator.isWindows()) {
+            executeWindows("start " + path);
+            return;
+        }
+
+        if (OSValidator.isMac()) {
+            executeMac("open " + path);
+            return;
+        }
     }
 }
