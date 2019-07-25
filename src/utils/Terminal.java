10c0c6ebe0d87b436e782354dd1d3ae39b91db36
Allef Douglas <allef.douglas@aftisolucoes.com.br>
2019-07-25 17:03:10 -0300

    Commit to pull

--- src/utils/Terminal.java
+++ src/utils/Terminal.java
@@ -1,18 +1,16 @@
 package utils;
 
 public class Terminal {
-    public static void exe(String path) throws Exception {
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "code " + path);
+    public static void executeWindows(String command) throws Exception {
+        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", command);
         pb.start();
     }
     
     public static void openVSCode(String path)  throws Exception {
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "code " + path);
-        pb.start();
+    	Terminal.executeWindows("code " + path);
     }
     
     public static void openFolder(String path)  throws Exception {
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start " + path);
-        pb.start();
+    	Terminal.executeWindows("start " + path);
     }
 }
